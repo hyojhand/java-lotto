a7c7d5250880be40d80b69f76a5f2b438588ff9c
손효재 <sonjw5128@gmail.com>
2023-03-11 04:05:18 +0900

    refactor: RandomNumberGenerator 클래스 싱글턴 패턴 적용 리팩토링

--- src/main/java/lotto/domain/RandomNumberGenerator.java
+++ src/main/java/lotto/domain/RandomNumberGenerator.java
@@ -2,18 +2,29 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RandomNumberGenerator {
 
     private static final List<Integer> NUMBERS = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList());
+    private static final RandomNumberGenerator RANDOM_NUMBER_GENERATOR = new RandomNumberGenerator();
 
-    public List<Integer> makeRandomNumbers() {
+    private RandomNumberGenerator() {
+    }
+
+    public static RandomNumberGenerator getInstance() {
+        return RANDOM_NUMBER_GENERATOR;
+    }
+
+    public Lotto makeRandomNumbers() {
         Collections.shuffle(NUMBERS);
 
-        return NUMBERS.stream()
+        Set<Integer> numbers = NUMBERS.stream()
                 .limit(6)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
+
+        return new Lotto(numbers);
     }
 }
